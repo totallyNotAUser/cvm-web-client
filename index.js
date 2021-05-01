ab3b5459b45658980359847989c0a906ac1fc7ff
totallyNotAUser <68023215+totallyNotAUser@users.noreply.github.com>
2021-05-01 21:31:51 +0000

    Add VM controlling (mouse and physical keyboard)

--- index.js
+++ index.js
@@ -56,11 +56,115 @@ class VMWebsocket {
 let currentConn = null;
 let currentUsername = null;
 
+function cancelEvent(e) {
+    e.stopPropagation();
+    if (e.preventDefault) e.preventDefault();
+    e.returnValue = false;
+}
+
 class VMDisplay {
     constructor() {
         this.size = { x: null, y: null };
         this.canvas = $('#vm-canvas').get(0);
         this.context = this.canvas.getContext('2d');
+        $(this.canvas).prop('tabindex', 1);
+        this.canvas.onclick = () => {
+            if ($('#turn-btn').is(':visible')) {
+                $('#turn-btn').click(); // get turn if we don't have turn
+            }
+            this.canvas.focus();
+        };
+        // mouse move handling
+        this.mouseLeft = false;
+        this.mouseRight = false;
+        this.mouseMiddle = false;
+
+        let updateMouseBtnVars = e => {
+            this.mouseLeft = (e.buttons & 0b001) == 0b001;
+            this.mouseRight = (e.buttons & 0b010) == 0b010;
+            this.mouseMiddle = (e.buttons & 0b100) == 0b100;
+        }
+        let sendMouse = (x, y) => {
+            let btnMask = 0;
+            if (this.mouseLeft) btnMask |= 1;
+            if (this.mouseMiddle) btnMask |= 2;
+            if (this.mouseRight) btnMask |= 4;
+            currentConn.sendGuac(['mouse', Math.floor(x).toString(), Math.floor(y).toString(), btnMask.toString()]);
+        };
+
+        this.canvas.oncontextmenu = e => {
+            cancelEvent(e);
+        };
+        let onmouse = e => {
+            cancelEvent(e);
+            updateMouseBtnVars(e);
+            let rect = e.target.getBoundingClientRect();
+            let x = e.clientX - rect.left;
+            let y = e.clientY - rect.top;
+            sendMouse(x, y);
+        };
+        this.canvas.onmouseup = onmouse;
+        this.canvas.onmousedown = onmouse;
+        this.canvas.onmousemove = onmouse;
+
+        // keyboard input handling
+        let isShiftDown = false;
+        const keycodeKeysyms = {
+            8:   [0xFF08], // backspace
+            9:   [0xFF09], // tab
+            13:  [0xFF0D], // enter
+            16:  [0xFFE1, 0xFFE1, 0xFFE2], // shift
+            17:  [0xFFE3, 0xFFE3, 0xFFE4], // ctrl
+            18:  [0xFFE9, 0xFFE9, 0xFE03], // alt
+            19:  [0xFF13], // pause/break
+            20:  [0xFFE5], // caps lock
+            27:  [0xFF1B], // escape
+            32:  [0x0020], // space
+            33:  [0xFF55], // page up
+            34:  [0xFF56], // page down
+            35:  [0xFF57], // end
+            36:  [0xFF50], // home
+            37:  [0xFF51], // left arrow
+            38:  [0xFF52], // up arrow
+            39:  [0xFF53], // right arrow
+            40:  [0xFF54], // down arrow
+            45:  [0xFF63], // insert
+            46:  [0xFFFF], // delete
+            91:  [0xFFEB], // left window key (hyper_l)
+            92:  [0xFF67], // right window key (menu key?)
+            93:  null,     // select key
+            112: [0xFFBE], // f1
+            113: [0xFFBF], // f2
+            114: [0xFFC0], // f3
+            115: [0xFFC1], // f4
+            116: [0xFFC2], // f5
+            117: [0xFFC3], // f6
+            118: [0xFFC4], // f7
+            119: [0xFFC5], // f8
+            120: [0xFFC6], // f9
+            121: [0xFFC7], // f10
+            122: [0xFFC8], // f11
+            123: [0xFFC9], // f12
+            144: [0xFF7F], // num lock
+            145: [0xFF14], // scroll lock
+            173: [45], // - key
+            191: [47], // / key
+            225: [0xFE03]  // altgraph (iso_level3_shift)
+        };
+        let onkb = (press, e) => {
+            cancelEvent(e);
+            if (e.repeat) return;
+            let keycode = e.keyCode;
+            if (keycode == 16) {
+                isShiftDown = (press == '1');
+            }
+            if (keycodeKeysyms.hasOwnProperty(keycode)) keycode = keycodeKeysyms[keycode][e.location] || keycodeKeysyms[keycode][0];
+            else if (!isShiftDown && keycode >= 65 && keycode <= 90) keycode = keycode | 0b00100000; // make letter non caps (ascii) if its A, Z or in between
+            console.log('key ', keycode);
+            currentConn.sendGuac(['key', keycode.toString(), press]);
+        }
+        this.canvas.onkeydown = e => onkb('1', e);
+        this.canvas.onkeyup = e => onkb('0', e);
     }
     setSize(x, y) {
         this.size.x = parseInt(x);
